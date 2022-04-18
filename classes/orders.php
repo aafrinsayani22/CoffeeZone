30f254f71064f4144f7e99a0a8098549af5e881f
Aafrin <Aafrin@AafrinNew.evcegabuibiu1g2uhg1qzvojzg.px.internal.cloudapp.net>
2022-04-18 04:37:06 +0000

    Aafrin Sayani (2030150) 2022-04-17 Created Plural objects for all tables.

--- classes/orders.php
+++ classes/orders.php
@@ -1,26 +1,27 @@
 <?php
 
+
+
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 require_once("collection.php");
 
-class customers extends collection 
+class orders extends collection 
 {
     function _contruct()
     {
         global $connection;
         
-        $sql = "call customer_all()";
+        $sql = "call order_all()";
         
         $PDOobject = $connection->prepare($sql);
         $PDOobject->execute();
         while($row = $PDOobject->fetch(PDO::FETCH_ASSOC))
         {
-            $customer = new customer($row["customer_id"], $row["firstname"],$row["lastname"], $row["address"],
-                    $row["city"], $row["province"],$row["postal_code"], $row["username"], $row["password"], $row["avatar"]);
-            $this->add($row["customer_id"], $customer);            
+            $order = new product($row["order_id"],$row["description"], $row["qty_sold"],$row["sold_price"], $row["comments"],$row["c_id"], $row["p_id"]);
+            $this->add($row["order_id"], $order);            
                     
         }
     }
